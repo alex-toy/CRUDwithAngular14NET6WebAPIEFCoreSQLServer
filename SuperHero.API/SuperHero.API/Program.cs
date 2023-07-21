655ef256fb6fec6470d843a96711a66ecc0998ff
alex-toy <alexei.80@hotmail.fr>
2023-07-21 18:48:01 +0200

    added controller

--- SuperHero.API/SuperHero.API/Program.cs
+++ SuperHero.API/SuperHero.API/Program.cs
@@ -1,3 +1,5 @@
+using SuperHeros.API.Repo;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
+
+string DefaultConnectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(DefaultConnectionString));
+
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins", policy =>
+{
+    policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
---
+++ SuperHero.API/SuperHero.API/Repo/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeros.API.Models;
+
+namespace SuperHeros.API.Repo
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
